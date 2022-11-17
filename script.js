ff709474c92dc45028f1080ade3420bb265e4e9e
Michael Mushrush <mushrushm@gmail.com>
2022-11-17 01:10:24 -0500

    85% percent finished project

--- script.js
+++ script.js
@@ -1,15 +1,47 @@
-const markAllAsReadEvent = () => {
-
-}
+$('.notif-header span').text($('.notif-unread').length)
+$('.notif.notif-unread .post-desc').after('<div class="dot"></div>')
 
 $('.notif-header p').on('click', (event) => {
-    //sets unread text counter to 0.
 
-    $('.notif-header span').text(0)
+    if (event.target.textContent === "Mark all as read") {
+        event.target.textContent = "Mark all as unread"
 
-    // $('notif notif-unread').removeClass('.notif-unread')
-    if ($('.notif').hasClass('notif-unread')) {
-        $('.notif').removeClass('notif-unread')
+        if ($('.notif').hasClass('notif-unread')) {
+            $('.notif.notif-unread').addClass('notif-read')
+            $('.notif.notif-unread').removeClass('notif-unread')
+            $('.dot').css('display', 'none')
+        }
     }
-    //
+    else {
+        event.target.textContent = "Mark all as read"
+        $('.dot').remove()
+
+        if ($('.notif').hasClass('notif-read')) {
+            $('.notif.notif-read').addClass('notif-unread')
+            $('.notif.notif-read').removeClass('notif-read')
+            $('.dot').css('display', 'inline-block')
+        }
+
+        //  console.log($('.dot'))
+        $('.notif.notif-unread .post-desc').after('<div class="dot"></div>')
+    }
+
+    $('.notif-header span').text($('.notif-unread').length)
+})
+
+$('.notif').on('click', (event) => {
+
+    if (event.target.classList.contains('notif-unread')) {
+
+        event.target.classList.remove('notif-unread')
+        event.target.classList.add('notif-read')
+        event.target.querySelector('.dot').remove()
+
+        let unreadLength = $('.notif-unread').length
+        $('.notif-header span').text(unreadLength)
+        // console.log("Length " + unreadLength)
+        if (unreadLength <= 0) {
+            $('.notif-header p').text("Mark all as unread")
+        }
+    }
 })
